890d31c34c43ef10d10b383ffc75af35afa0291b
Andrew Ayers <andrew.ayers@nobistech.net>
2015-08-05 15:03:12 -0700

    Mods to remove server-specific directory structure.
    Signed-off-by: Andrew Ayers <andrew.ayers@nobistech.net>

--- glue/config/config.php
+++ glue/config/config.php
@@ -1,11 +1,11 @@
 <?php namespace MyFramework;
 
     $routes = array(
-        '/andrew/php/glue/' => 'index',
-        '/andrew/php/glue/capture' => 'capture',
-        '/andrew/php/glue/(\d+)' => 'capture',
-        '/andrew/php/glue/([a-zA-Z0-9_]*)/(\d+)' => 'capture',
-        '/andrew/php/glue/poster' => 'poster',
-        '/andrew/php/glue/put' => 'extended',
-        '/andrew/php/glue/delete/([a-zA-Z0-9_]*)' => 'extended',
+        '/php/glue/' => 'index',
+        '/php/glue/capture' => 'capture',
+        '/php/glue/(\d+)' => 'capture',
+        '/php/glue/([a-zA-Z0-9_]*)/(\d+)' => 'capture',
+        '/php/glue/poster' => 'poster',
+        '/php/glue/put' => 'extended',
+        '/php/glue/delete/([a-zA-Z0-9_]*)' => 'extended',
     );
